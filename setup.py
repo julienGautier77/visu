d2e8f55ed450669d347f8a8db4c89de156929c01
julienGautier <julien.gautier@ensta.fr>
2019-03-19 15:09:45 +0100

    modified setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ Created on Mon Mar 18 14:06:58 2019
 from setuptools import setup, find_packages
 import visu
 
-setup(name='visu',version=visu.__version__,author='Julien Gautier',author_email='julien.gautier@ensta.fr',description='Data visualization',
-      long_description=open('README.md').read(),python_requires='<4')
+setup(name='visu',version=visu.__version__,packages=['visu'],author='Julien Gautier',author_email='julien.gautier@ensta.fr',
+      description='Data visualization',
+      long_description=open('README.md').read(),url='https://github.com/julienGautier77/visu')
