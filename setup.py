2362798046b9ee5addba5ba95dfc4a13c42023ad
julien gautier <julien.gautier@ensta.fr>
2023-08-10 10:15:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from setuptools import setup, find_packages
 #import visu
 This_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (This_directory / "README.md").read_text()
 setup(name='visu',version=2023.07,
       packages=find_packages(),
       author='Julien Gautier',
